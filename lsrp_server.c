3bd13b97341858047e43053441f4cd8bfa59421c
pavelavl <sfdrus2004@gmail.com>
2025-11-06 22:47:58 +0300

    feat: thread pool; recv once

--- lsrp_server.c
+++ lsrp_server.c
@@ -10,8 +10,12 @@
 #include <sys/resource.h>
 #include <pthread.h>
 #include <signal.h>
+#include <sys/epoll.h>
 
-#define LISTEN_BACKLOG 1024
+#define LISTEN_BACKLOG 4096
+#define RECV_BUFFER_SIZE 8192
+#define THREAD_POOL_SIZE 16
+#define MAX_EVENTS 512
 
 static inline void uint32_to_be(uint32_t val, uint8_t *bytes) {
     bytes[0] = (val >> 24) & 0xFF;
@@ -26,65 +30,127 @@ static inline uint32_t be_to_uint32(const uint8_t *bytes) {
 }
 
 typedef struct {
+    int sock;
+    char buffer[RECV_BUFFER_SIZE];
+    size_t buffer_pos;
+    size_t expected_len;
+} client_state_t;
+
+typedef struct work_item {
     int client_sock;
     lsrp_handler_t handler;
-} thread_args_t;
+    struct work_item *next;
+} work_item_t;
 
-static void* handle_client(void* arg) {
-    thread_args_t* args = (thread_args_t*)arg;
-    int client_sock = args->client_sock;
-    lsrp_handler_t handler = args->handler;
-    free(args);
+typedef struct {
+    pthread_mutex_t lock;
+    pthread_cond_t cond;
+    work_item_t *head;
+    work_item_t *tail;
+    int shutdown;
+} work_queue_t;
 
-    pthread_detach(pthread_self());
+static work_queue_t work_queue = {
+    .lock = PTHREAD_MUTEX_INITIALIZER,
+    .cond = PTHREAD_COND_INITIALIZER,
+    .head = NULL,
+    .tail = NULL,
+    .shutdown = 0
+};
 
-    // Отключаем Nagle для минимальной latency
-    int flag = 1;
-    setsockopt(client_sock, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag));
-
-    // Устанавливаем размеры буферов
-    int buf_size = 131072; // 128KB
-    setsockopt(client_sock, SOL_SOCKET, SO_RCVBUF, &buf_size, sizeof(buf_size));
-    setsockopt(client_sock, SOL_SOCKET, SO_SNDBUF, &buf_size, sizeof(buf_size));
-
-    // Обрабатываем только ОДИН запрос (как HTTP без keep-alive)
-    uint8_t header[LSRP_MAGIC_LEN + 4];
-    ssize_t recv_len = recv(client_sock, header, sizeof(header), MSG_WAITALL);
-
-    if (recv_len != sizeof(header)) {
+static void enqueue_work(int client_sock, lsrp_handler_t handler) {
+    work_item_t *item = malloc(sizeof(work_item_t));
+    if (!item) {
         close(client_sock);
-        return NULL;
+        return;
     }
-
-    // Check magic
-    if (memcmp(header, LSRP_MAGIC, LSRP_MAGIC_LEN) != 0) {
-        close(client_sock);
-        return NULL;
+    
+    item->client_sock = client_sock;
+    item->handler = handler;
+    item->next = NULL;
+    
+    pthread_mutex_lock(&work_queue.lock);
+    if (work_queue.tail) {
+        work_queue.tail->next = item;
+    } else {
+        work_queue.head = item;
     }
+    work_queue.tail = item;
+    pthread_cond_signal(&work_queue.cond);
+    pthread_mutex_unlock(&work_queue.lock);
+}
 
-    uint32_t params_len = be_to_uint32(header + LSRP_MAGIC_LEN);
-    if (params_len > LSRP_MAX_PARAMS_LEN) {
-        close(client_sock);
-        return NULL;
+static work_item_t* dequeue_work(void) {
+    pthread_mutex_lock(&work_queue.lock);
+    
+    while (!work_queue.head && !work_queue.shutdown) {
+        pthread_cond_wait(&work_queue.cond, &work_queue.lock);
     }
-
-    // Read params
-    char *params = malloc(params_len + 1);
-    if (!params) {
-        close(client_sock);
+    
+    if (work_queue.shutdown) {
+        pthread_mutex_unlock(&work_queue.lock);
         return NULL;
     }
     
-    recv_len = recv(client_sock, params, params_len, MSG_WAITALL);
-    if (recv_len != (ssize_t)params_len) {
-        free(params);
-        close(client_sock);
-        return NULL;
+    work_item_t *item = work_queue.head;
+    work_queue.head = item->next;
+    if (!work_queue.head) {
+        work_queue.tail = NULL;
     }
-    params[params_len] = '\0';
+    
+    pthread_mutex_unlock(&work_queue.lock);
+    return item;
+}
 
-    // Call handler
-    lsrp_request_t req = { .params = params, .params_len = params_len };
+static void process_request(int client_sock, lsrp_handler_t handler) {
+    int flag = 1;
+    setsockopt(client_sock, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag));
+    
+    #ifdef TCP_QUICKACK
+    setsockopt(client_sock, IPPROTO_TCP, TCP_QUICKACK, &flag, sizeof(flag));
+    #endif
+
+    int buf_size = 262144;
+    setsockopt(client_sock, SOL_SOCKET, SO_RCVBUF, &buf_size, sizeof(buf_size));
+    setsockopt(client_sock, SOL_SOCKET, SO_SNDBUF, &buf_size, sizeof(buf_size));
+
+    char buffer[RECV_BUFFER_SIZE];
+    ssize_t total_recv = recv(client_sock, buffer, sizeof(buffer), 0);
+    
+    if (total_recv < (ssize_t)(LSRP_MAGIC_LEN + 4)) {
+        close(client_sock);
+        return;
+    }
+
+    if (memcmp(buffer, LSRP_MAGIC, LSRP_MAGIC_LEN) != 0) {
+        close(client_sock);
+        return;
+    }
+
+    uint32_t params_len = be_to_uint32((uint8_t*)buffer + LSRP_MAGIC_LEN);
+    if (params_len > LSRP_MAX_PARAMS_LEN) {
+        close(client_sock);
+        return;
+    }
+
+    size_t expected_total = LSRP_MAGIC_LEN + 4 + params_len;
+    
+    while ((size_t)total_recv < expected_total) {
+        ssize_t n = recv(client_sock, buffer + total_recv, 
+                        sizeof(buffer) - total_recv, 0);
+        if (n <= 0) {
+            close(client_sock);
+            return;
+        }
+        total_recv += n;
+    }
+
+    char *params = buffer + LSRP_MAGIC_LEN + 4;
+    char params_copy[LSRP_MAX_PARAMS_LEN + 1];
+    memcpy(params_copy, params, params_len);
+    params_copy[params_len] = '\0';
+
+    lsrp_request_t req = { .params = params_copy, .params_len = params_len };
     lsrp_response_t resp = { .status = 1, .data = NULL, .data_len = 0 };
     handler(&req, &resp);
 
@@ -93,7 +159,6 @@ static void* handle_client(void* arg) {
         resp.data_len = 14;
     }
 
-    // Prepare and send response (одна аллокация, одна отправка)
     size_t total_len = LSRP_MAGIC_LEN + 1 + 4 + resp.data_len;
     uint8_t *response = malloc(total_len);
     
@@ -105,14 +170,25 @@ static void* handle_client(void* arg) {
             memcpy(response + LSRP_MAGIC_LEN + 5, resp.data, resp.data_len);
         }
         
-        // Отправляем весь ответ одним вызовом
         send(client_sock, response, total_len, MSG_NOSIGNAL);
         free(response);
     }
 
-    free(params);
     if (resp.data) free(resp.data);
     close(client_sock);
+}
+
+static void* worker_thread(void* arg) {
+    (void)arg;
+    
+    while (1) {
+        work_item_t *item = dequeue_work();
+        if (!item) break;
+        
+        process_request(item->client_sock, item->handler);
+        free(item);
+    }
+    
     return NULL;
 }
 
@@ -124,11 +200,18 @@ int lsrp_server_start(int port, lsrp_handler_t handler) {
 
     signal(SIGPIPE, SIG_IGN);
 
-    // Increase file descriptor limit
     struct rlimit rl;
     if (getrlimit(RLIMIT_NOFILE, &rl) == 0) {
         rl.rlim_cur = (rl.rlim_max < 65536) ? rl.rlim_max : 65536;
         setrlimit(RLIMIT_NOFILE, &rl);
+    }
+
+    pthread_t workers[THREAD_POOL_SIZE];
+    for (int i = 0; i < THREAD_POOL_SIZE; i++) {
+        if (pthread_create(&workers[i], NULL, worker_thread, NULL) != 0) {
+            fprintf(stderr, "Failed to create worker thread %d\n", i);
+            return -1;
+        }
     }
 
     int server_sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -139,10 +222,14 @@ int lsrp_server_start(int port, lsrp_handler_t handler) {
 
     int opt = 1;
     setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-    setsockopt(server_sock, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt)); // Для параллельности
+    setsockopt(server_sock, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt));
 
-    // Буферы для сервера
-    int buf_size = 131072;
+    #ifdef TCP_DEFER_ACCEPT
+    int timeout = 1;
+    setsockopt(server_sock, IPPROTO_TCP, TCP_DEFER_ACCEPT, &timeout, sizeof(timeout));
+    #endif
+
+    int buf_size = 262144;
     setsockopt(server_sock, SOL_SOCKET, SO_RCVBUF, &buf_size, sizeof(buf_size));
     setsockopt(server_sock, SOL_SOCKET, SO_SNDBUF, &buf_size, sizeof(buf_size));
 
@@ -163,13 +250,8 @@ int lsrp_server_start(int port, lsrp_handler_t handler) {
         return -4;
     }
 
-    fprintf(stderr, "LSRP server listening on port %d\n", port);
-
-    // Атрибуты потоков (создаем один раз)
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-    pthread_attr_setstacksize(&attr, 256 * 1024); // 256KB stack
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+    fprintf(stderr, "LSRP server with thread pool (%d workers) listening on port %d\n", 
+            THREAD_POOL_SIZE, port);
 
     while (1) {
         struct sockaddr_in client_addr;
@@ -181,22 +263,16 @@ int lsrp_server_start(int port, lsrp_handler_t handler) {
             continue;
         }
 
-        thread_args_t* args = malloc(sizeof(thread_args_t));
-        if (!args) {
-            close(client_sock);
-            continue;
-        }
-        args->client_sock = client_sock;
-        args->handler = handler;
-
-        pthread_t thread;
-        if (pthread_create(&thread, &attr, handle_client, args) != 0) {
-            free(args);
-            close(client_sock);
-        }
+        enqueue_work(client_sock, handler);
     }
 
-    pthread_attr_destroy(&attr);
+    work_queue.shutdown = 1;
+    pthread_cond_broadcast(&work_queue.cond);
+    
+    for (int i = 0; i < THREAD_POOL_SIZE; i++) {
+        pthread_join(workers[i], NULL);
+    }
+
     close(server_sock);
     return 0;
 }
